5740548ba8cb48eb378cd2555620e24d69d08b0e
Bjarne Christel <develop.christel@gmail.com>
2024-01-10 11:50:50 +0100

    upd: test case processing

--- spring-apps/testing/src/main/java/com/example/testing/adapter/messaging/EventListener.java
+++ spring-apps/testing/src/main/java/com/example/testing/adapter/messaging/EventListener.java
@@ -2,7 +2,6 @@ package com.example.testing.adapter.messaging;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.regex.Pattern;
 
 import org.springframework.kafka.annotation.KafkaListener;
 
@@ -24,38 +23,26 @@ public class EventListener {
 	public void listen(String message) {
 		System.out.println("DEBUGINFO Nachricht: " + message);
 		 
-		/*String parts[] = message.split(Pattern.quote("/"));
-		 
-		String event = parts[0];
-		String payload = parts[1];
-		 
-		System.out.println("EVENT: " + event);
-		System.out.println("PAYLOAD: " + payload);*/
-		 
 //		try {
 //			Thread.sleep(10000);
 //		} catch (InterruptedException e1) {
 //			// TODO Auto-generated catch block
 //			e1.printStackTrace();
 //		}
-		 
-		/*if (event.equals("statusChanged")) {
 
-			ObjectMapper mapper = new ObjectMapper();
-			TestCaseTO[] testCaseListeTOArray = null;
-			
-			try {
-				testCaseListeTOArray = mapper.readValue(payload, TestCaseTO[].class);
-			} catch (JsonProcessingException e) {
-				System.out.println("Interner Fehler bei der Eventverarbeitung");
-				e.printStackTrace();
-			}
-			
-			Collection<TestCaseTO> testCaseListeTO = Arrays.asList(testCaseListeTOArray);
-			
-			if (!testCaseService.processTestCases(testCaseListeTO))
-				System.out.println("Verarbeitung fehlgeschlagen!"); 
-				
-		 }*/
+		ObjectMapper mapper = new ObjectMapper();
+		TestCaseTO[] testCaseListeTOArray = null;
+		
+		try {
+			testCaseListeTOArray = mapper.readValue(message, TestCaseTO[].class);
+		} catch (JsonProcessingException e) {
+			System.out.println("Interner Fehler bei der Eventverarbeitung");
+			e.printStackTrace();
+		}
+		
+		Collection<TestCaseTO> testCaseListeTO = Arrays.asList(testCaseListeTOArray);
+		
+		if (!testCaseService.processTestCases(testCaseListeTO))
+			System.out.println("Verarbeitung fehlgeschlagen!"); 
 	 }
 }
